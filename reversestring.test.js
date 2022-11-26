4c2b3f164f475a0e01119fd991d3b7a3ebc80ee0
Gutri Rahmad Zuwa <gutri363696@gmail.com>
2022-11-27 03:43:21 +0700

    PART 1 & 2

---
+++ reversestring.test.js
@@ -0,0 +1,23 @@
+const reverseString = require('./reversestring');
+
+test('reverseString function exists', () => {
+  expect(reverseString).toBeDefined();
+});
+
+test('String reverses', () => {
+  expect(reverseString('hello')).toEqual('olleh');
+});
+
+test('String reverses', () => {
+  // complete with another equal example
+  expect(reverseString('good')).toEqual('doog');
+});
+
+test('String reverses with uppercase', () => {
+  expect(reverseString('Hello')).toEqual('olleh');
+});
+
+test('String reverses with uppercase', () => {
+  // complete with another uppercase equal example
+  expect(reverseString('Good')).toEqual('doog');
+});
